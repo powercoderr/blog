d3efa034513bdd9ae4d5dbbc0eb38e341b04de51
powercoderr <dena.djayusman.tif19@polban.ac.id>
2021-09-15 14:11:06 +0700

    Update : Using blade templating in views

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,22 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>Blog</title>
-</head>
-<body>
+@extends('layout')
+@section('content')
     <h1 id="blog-headline">Welcome to My Blog</h1>
     @foreach ($posts as $post)
         <article>
             <h1><a href="/blog/posts/{{ $post->slug }}">{{ $post->title }}</a></h1>
-            <div>
-                {{ $post->excerpt }}
-            </div>
+            <div>{{ $post->excerpt }}</div>
         </article>     
     @endforeach
-
-</body>
-</html>
+@endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>Post</title>
-</head>
-<body>
+@extends('layout')
+@section('content')
     <article>
         <h1>{{ $post->title }}</h1>
         <div>{!! $post->body !!}</div>
         <a href="/blog">Back to home ...</a>
     </article>
-    
-    
-</body>
-</html>
+@endsection
